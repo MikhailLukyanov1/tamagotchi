a6f54ea61f2ce396856c2f61d49d5cf0aa66125a
MikhailLukyanov1 <lukyanov.m@etoncollege.org.uk>
2023-01-24 10:14:03 +0000

    Initial Commit

---
+++ tamagotchi/tamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tamagotchiApp.swift
+//  tamagotchi
+//
+//  Created by Luk'yanov, Mikhail (JMOB) on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct tamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
